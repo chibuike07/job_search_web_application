0b32d8b287a6b798ee00e1115987dc1d343ce213
hobbyluv <princewillchime43@gmail.com>
2020-07-15 23:14:23 +0100

    installed the gh page to the dev dependency

--- src/component/Yelp.js
+++ src/component/Yelp.js
@@ -31,7 +31,6 @@ const yelp = {
             };
           });
         }
-        console.log(jsonResponse);
       });
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from './App';
 import Parent from "./component/Parent.component";
 import * as serviceWorker from "./serviceWorker";
 
