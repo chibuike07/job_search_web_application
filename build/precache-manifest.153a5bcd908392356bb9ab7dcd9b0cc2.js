62d54a2f5ead3b573eeeec76c11178c46d8f4213
hobbyluv <princewillchime43@gmail.com>
2020-07-15 23:31:38 +0100

    deployed app success

---
+++ build/precache-manifest.153a5bcd908392356bb9ab7dcd9b0cc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50c9b6b0acbebd241af3950c633eb9f",
+    "url": "/job_search_web_application/index.html"
+  },
+  {
+    "revision": "77592529a230a7350385",
+    "url": "/job_search_web_application/static/css/main.ce04adbd.chunk.css"
+  },
+  {
+    "revision": "f6126554a7b682897b58",
+    "url": "/job_search_web_application/static/js/2.57506006.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/job_search_web_application/static/js/2.57506006.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77592529a230a7350385",
+    "url": "/job_search_web_application/static/js/main.99daf9b4.chunk.js"
+  },
+  {
+    "revision": "f28201b461072eeb0d97",
+    "url": "/job_search_web_application/static/js/runtime-main.ac2ad370.js"
+  }
+]);
